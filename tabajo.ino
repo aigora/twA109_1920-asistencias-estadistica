5f86ceb9c7f7bc58b0917eab9b0c4a9503677216
eduardo-luque <61051891+eduardo-luque@users.noreply.github.com>
2020-03-13 12:24:38 +0100

    Update tabajo.ino

--- tabajo.ino
+++ tabajo.ino
@@ -1,1 +1,20 @@
-asdasd
+int sensorPin1 = A0;    // select the input pin for the sensor
+int sensorPin2 = A1;    // select the input pin for the sensor
+int sensorValue1 = 0;  // variable que se guarda, proviene del sensor exterior
+int sensorValue2 = 0;  // variable que se guarda, proviene del sensor interior
+
+void setup() {
+  // parte de el código que solo se procesa una vez:
+ pinMode(sensorPin1, INTPUT);
+ pinMode(sensorPin2, INTPUT);
+}
+
+
+void loop() {
+  // parte del código que se repite en bucle:
+  sensorValue = analogRead(sensorPin1);
+  sensorValue = analogRead(sensorPin2);
+
+  
+
+}
