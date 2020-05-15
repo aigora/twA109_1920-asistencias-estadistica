99ab2f5736190361a1b987955f617b261df54627
Gabriel-Rpascual <61052061+Gabriel-Rpascual@users.noreply.github.com>
2020-05-15 16:18:51 +0200

    Update Fichero.c

--- Fichero.c
+++ Fichero.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-FILE * crear_fichero (void);
-
+#include <string.h>
+#include "SerialPort.h"
+#define MAX_DATA_LENGTH 255
+// Funciones prototipo
+void autoConnect(SerialPort *arduino,char*);
 int main(void)
 {
-  FILE *mifichero;
+ //Arduino SerialPort object
+ SerialPort *arduino;
+ // Puerto serie en el que está Arduino
+ char* portName = "\\\\.\\COM3";
+ // Buffer para datos procedentes de Arduino
+ char incomingData[MAX_DATA_LENGTH];
 
-   mifichero = crear_fichero();
-
-  return 0;
+ // Crear estructura de datos del puerto serie
+ arduino = (SerialPort *)malloc(sizeof(SerialPort));
+ // Apertura del puerto serie
+ Crear_Conexion(arduino,portName);
+ autoConnect(arduino,incomingData);
 }
-
-FILE * crear_fichero (void)
+void autoConnect(SerialPort *arduino,char *incomingData)
 {
-  FILE *mifichero;
-  int cierre;
+ int readResult;
 
-  mifichero = fopen ("Arduino.txt","w"); //Destructiva 
-  if (mifichero==NULL)
-    printf ("Error no se ha podido abrir el fichero\n");
-  else
-  {
-     cierre = fclose (mifichero);
-
-     if (cierre == EOF);
-	    // Acciones asociadas a la imposibilidad de cerrar el fichero.
-     else;
-        // Acciones asociadas una vez cerrado el fichero
-  }
-  return mifichero;
+// Espera la conexión con Arduino
+while (!isConnected(arduino))
+{
+Sleep(100);
+Crear_Conexion(arduino,arduino->portName);
+}
+ //Comprueba si arduino está connectado
+if (isConnected(arduino))
+{
+printf ("Conectado con Arduino en el puerto %s\n",arduino-
+>portName);
+}
+ // Bucle de la aplicación
+while (isConnected(arduino))
+{
+ readResult = readSerialPort(arduino,incomingData,
+MAX_DATA_LENGTH);
+if (readResult!=0)
+ printf("%s - %d\n", incomingData,readResult);
+ Sleep(10);
+}
+if (!isConnected(arduino))
+ printf ("Se ha perdido la conexión con Arduino\n");
 }
