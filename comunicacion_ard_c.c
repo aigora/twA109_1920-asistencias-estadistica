849ca9352e5f3bb26faeea60cf06ebf95262b3a7
eduardo-luque <61051891+eduardo-luque@users.noreply.github.com>
2020-05-18 19:59:25 +0200

    Update comunicacion_ard_c.c

--- comunicacion_ard_c.c
+++ comunicacion_ard_c.c
@@ -5,6 +5,9 @@
 #define MAX_DATA_LENGTH 255
 // Funciones prototipo
 void autoConnect(SerialPort *arduino,char*);
+FILE * crear_fichero (void);
+void leer_fichero (FILE *);
+
 int main(void)
 {
  //Arduino SerialPort object
@@ -42,8 +45,56 @@ void autoConnect(SerialPort *arduino,char *incomingData)
     MAX_DATA_LENGTH);
     if (readResult!=0)
        printf("%s - %d\n", incomingData,readResult);
+       FILE *mifichero;
+       mifichero = crear_fichero();//add
+       leer_fichero (mifichero);//add
     Sleep(10);
   }
  if (!isConnected(arduino))
    printf ("Se ha perdido la conexión con Arduino\n");
+}
+void leer_fichero (FILE *mifichero)
+{
+  int n,i,cierre;
+  mifichero = fopen ("datos.txt","r");
+  
+  if (mifichero==NULL)
+    printf ("Error no se ha podido abrir el fichero\n");
+  else
+  {
+     fscanf (mifichero,"%d",&n); 
+     while (!feof(mifichero)) 
+     {
+       printf ("Acabo de leer=%d\n",n);
+       fscanf (mifichero,"%d",&n);
+     }
+    cierre = fclose (mifichero);
+    if (cierre == EOF)
+        printf ("Problemas al cerrar\n");// Acciones asociadas a la imposibilidad de cerrar el fichero.
+    else
+        printf ("Todo ok\n");
+         // Acciones asociadas una vez cerrado el fichero
+  }
+  return;
+}
+
+FILE * crear_fichero (void)
+{
+  FILE *mifichero;
+  int i,cierre;
+
+  mifichero = fopen ("datos.txt","w"); //Destructiva 
+  if (mifichero==NULL)
+    printf ("Error no se ha podido abrir el fichero\n");
+  else
+  {
+     //acciones a realizar en el fichero ****************
+     cierre = fclose (mifichero);
+     if (cierre == EOF)
+        printf ("Problemas al cerrar\n");// Acciones asociadas a la imposibilidad de cerrar el fichero.
+    else
+        printf ("Todo ok\n");
+         // Acciones asociadas una vez cerrado el fichero
+  }
+  return mifichero;
 }
