43dc5dd350fea00c8310ca41c1e2c5cafb62c832
eduardo-luque <61051891+eduardo-luque@users.noreply.github.com>
2020-05-23 18:14:18 +0200

    Update tabajo.ino

--- tabajo.ino
+++ tabajo.ino
@@ -6,12 +6,19 @@ int sensorValue2 = 0;  // variable que se guarda, proviene del sensor interior
 int sensorMax = 0;           // valor máximo del sensor, parte del 0
 int c=0;               // contador de personas 
 const int ButtonPin = 2;
-
+/*
+const int EchoPin = 5;
+const int TriggerPin = 6;
+*/
 void setup() {
   // parte de el código que solo se procesa una vez:
+  /*
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT)
+  */
  pinMode(2, INPUT);// declaración del sensor exterior como entrada
  pinMode(11, INPUT);// declaración del sensor interior como entrada
- pinMode(13, OUTPUT);       //declaracion del pin del LED que nos indica el periodo de calibración como salida
+ pinMode(ledpin, OUTPUT);       //declaracion del pin del LED que nos indica el periodo de calibración como salida
  digitalWrite(13, HIGH);    // al inicio del programa este LED aparecerá encendido, indicando que se inicia el periodo de calibración de distancia de los sensores
  Serial.begin(9600);    // configuración de serie
   // calibración durante los primeros 5 segundos desde el inicio del programa
@@ -36,6 +43,12 @@ void setup() {
 
 void loop() {
   // parte del código que se repite en bucle:
+  /*
+  int cm = ping(TriggerPin, EchoPin);
+  Serial.print("distancia: ");
+  Serial.println(cm);
+  delay(500);
+   */
   sensorValue1 = analogRead(sensorPin1);
 
   Serial.println(c); // mostrar en pantalla el contador de personas
@@ -60,3 +73,18 @@ void loop() {
  }
   delay (200);
 } 
+
+int ping (int TriggerPin, int EchoPin)
+{
+  long duration, distanceCm;
+  
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, LOW);
+  
+  duration = pulseIn(EchoPin, HIGH);
+  distanceCm = duration * 10 / 292 / 2;
+    return distanceCm;
+}
