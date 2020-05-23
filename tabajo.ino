165c13a6f9dd92f22a8dd6fcde3624a3d70c92d4
Gabriel-Rpascual <61052061+Gabriel-Rpascual@users.noreply.github.com>
2020-05-23 18:29:12 +0200

    Update tabajo.ino

--- tabajo.ino
+++ tabajo.ino
@@ -1,21 +1,20 @@
-int sensorPin1 = 2;    // declaración del sensor exterior conectado en el analógico 0
+//int sensorPin1 = 2;    // declaración del sensor exterior conectado en el analógico 0
 int sensorPin2 = 11;    // declaración del sensor interior conectado en el analógico 1
 int ledPin = 13;        // en el pin 13 se encuentra el LED que nos indica el periodo de calibración 
 int sensorValue1 = 0;  // variable que se guarda, proviene del sensor exterior
 int sensorValue2 = 0;  // variable que se guarda, proviene del sensor interior
 int sensorMax = 0;           // valor máximo del sensor, parte del 0
 int c=0;               // contador de personas 
-const int ButtonPin = 2;
-/*
-const int EchoPin = 5;
-const int TriggerPin = 6;
-*/
+//const int ButtonPin = 2;
+const int EchoPin = 2;
+const int TriggerPin = 3;
+
 void setup() {
   // parte de el código que solo se procesa una vez:
-  /*
+  
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT)
-  */
+  
  pinMode(2, INPUT);// declaración del sensor exterior como entrada
  pinMode(11, INPUT);// declaración del sensor interior como entrada
  pinMode(ledpin, OUTPUT);       //declaracion del pin del LED que nos indica el periodo de calibración como salida
@@ -23,7 +22,7 @@ void setup() {
  Serial.begin(9600);    // configuración de serie
   // calibración durante los primeros 5 segundos desde el inicio del programa
   while (millis() < 5000) {
-    sensorValue1 = analogRead(sensorPin1); // esta calibracion se realiza con  el sensor exterior
+    sensorValue1 = ping(TriggerPin, EchoPin); // esta calibracion se realiza con  el sensor exterior
 
     // valor máximo captado por el sensor, se compara con el 0 elegido previamente(sensorMax) y a partir de ahi se reasigna segun lo que capta el sensor
     if (sensorValue1 > sensorMax) 
@@ -43,17 +42,17 @@ void setup() {
 
 void loop() {
   // parte del código que se repite en bucle:
-  /*
+  
   int cm = ping(TriggerPin, EchoPin);
   Serial.print("distancia: ");
   Serial.println(cm);
   delay(500);
-   */
-  sensorValue1 = analogRead(sensorPin1);
+   
+  sensorValue1 = cm;
 
   Serial.println(c); // mostrar en pantalla el contador de personas
   
- if ( sensorValue1 < sensorMax-50)     // toda distancia detectada menor que el máximo de la calibracion con un margen de 50 mm lo cuenta como una persona que pasa
+ if ( sensorValue1 < sensorMax-7)     // toda distancia detectada menor que el máximo de la calibracion con un margen de 50 mm lo cuenta como una persona que pasa
   { 
     digitalWrite(13, HIGH);          // encender luz LED   
     c++; // incremento del contador
